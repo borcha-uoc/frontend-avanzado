111a6b8d2c2307f54e8c646477887567410582c4
Bruno Orcha García <borcha@uoc.edu>
2019-10-14 22:41:37 +0200

    Se crea formulario reactivo para edición de datos personales

--- src/app/views/profile/profile-personal-data/profile-personal-data.component.ts
+++ src/app/views/profile/profile-personal-data/profile-personal-data.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { User } from 'src/app/shared/models/user.model';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service'
 
 @Component({
   selector: 'app-profile-personal-data',
   templateUrl: './profile-personal-data.component.html',
   styleUrls: ['./profile-personal-data.component.scss']
 })
-export class ProfilePersonalDataComponent implements OnInit {
+export class ProfilePersonalDataComponent {
+  user: User;
+  personalDataForm: FormGroup;
 
-  constructor() { }
+  
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService) {
+
+    this.user = authenticationService.currentUser;
+
+    this.personalDataForm = this.formBuilder.group({
+      name: [this.user.name],
+      surname: [this.user.surname],
+      birthdate: [this.user.birthdate],
+      phone: [this.user.phone],
+      phone2: [this.user.phone2],
+      documentType: [this.user.documentType],
+      documentNumber: [this.user.documentNumber],
+      address: this.formBuilder.group({
+        street: [this.user.address.street],
+        province: [this.user.address.province],
+        municipe: [this.user.address.municipe],
+      }),
+      license: [this.user.license],
+      aboutMe: [this.user.aboutMe],
+      otherCompetences: [this.user.otherCompetences]
+    });
+
+  save() {
+    console.log(this.personalDataForm.value)
+  }
   }
 
 }
