7ffbac4a12d31727d441b854e18ced8c05d1cdb3
Bruno Orcha García <borcha@uoc.edu>
2019-10-15 00:09:42 +0200

    Se crea pantalla inicial de listado de idiomas y borrado

--- src/app/views/profile/profile-languages/profile-languages.component.ts
+++ src/app/views/profile/profile-languages/profile-languages.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service'
+import { User } from 'src/app/shared/models/user.model';
 
 @Component({
   selector: 'app-profile-languages',
   templateUrl: './profile-languages.component.html',
   styleUrls: ['./profile-languages.component.scss']
 })
-export class ProfileLanguagesComponent implements OnInit {
+export class ProfileLanguagesComponent {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit() {
+  constructor(private authenticationService: AuthenticationService) {
+    this.user = this.authenticationService.currentUser;
   }
 
+  remove(language) {
+    if (!confirm(`¿Eliminar el idioma: ${language.name.name}?`)) {
+      return;
+    }
+    
+    this.user.languages = this.user.languages.filter(s => s.uid != language.uid);
+    this.authenticationService.saveUser(this.user);
+  }
 }
