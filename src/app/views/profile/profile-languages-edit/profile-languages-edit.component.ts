9122f0b7dbb29804fd230b47a10494df506dfb2c
Bruno Orcha García <borcha@uoc.edu>
2019-10-19 16:16:45 +0200

    Se rellenan los desplegables usando el languages service

--- src/app/views/profile/profile-languages-edit/profile-languages-edit.component.ts
+++ src/app/views/profile/profile-languages-edit/profile-languages-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Language, LanguageLevel, LanguageName } from 'src/app/shared/models/language.model';
+import { LanguagesService } from 'src/app/shared/services/languages.service'
 
 @Component({
   selector: 'app-profile-languages-edit',
@@ -9,13 +10,20 @@ import { Language, LanguageLevel, LanguageName } from 'src/app/shared/models/lan
 })
 export class ProfileLanguagesEditComponent {
 
+  languageNames;
+  languageLevels;
 
   editing: boolean;
   languagesForm: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder)
+    private formBuilder: FormBuilder,
+    private languagesService: LanguagesService)
   {
+    // Cargar opciones de los desplegables
+    this.languagesService.getLanguageNames().then(languageNames => this.languageNames = languageNames);
+    this.languagesService.getLanguageLevels().then(languageLevels => this.languageLevels = languageLevels);
+
     let language = new Language();
     this.languagesForm = this.createFormGroup(language);
   }
@@ -33,5 +41,9 @@ export class ProfileLanguagesEditComponent {
 
   save() {
     console.log(this.languagesForm.value);
+  }
+
+  compareById(c1: any, c2:any): boolean {
+    return c1 && c2 ? c1.id === c2.id : c1 === c2; 
   }
 }
