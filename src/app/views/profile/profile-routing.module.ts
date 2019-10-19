5158c73c2cdfb3f09fa504215e5d568517886c02
Bruno Orcha García <borcha@uoc.edu>
2019-10-19 04:50:45 +0200

    Se crean nuevas rutas para los menús de listado y edición de estudios e idiomas

--- src/app/views/profile/profile-routing.module.ts
+++ src/app/views/profile/profile-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 import { ProfilePersonalDataComponent } from './profile-personal-data/profile-personal-data.component';
 import { ProfileStudiesComponent } from './profile-studies/profile-studies.component';
+import { ProfileStudiesEditComponent } from './profile-studies-edit/profile-studies-edit.component';
 import { ProfileLanguagesComponent } from './profile-languages/profile-languages.component';
+import { ProfileLanguagesEditComponent } from './profile-languages-edit/profile-languages-edit.component';
 
 const routes: Routes = [
   {
@@ -19,8 +21,24 @@ const routes: Routes = [
     component: ProfileStudiesComponent
   },
   {
+    path: 'studies/new',
+    component: ProfileStudiesEditComponent
+  },
+  {
+    path: 'studies/:uid/edit',
+    component: ProfileStudiesEditComponent
+  },
+  {
     path: 'languages',
     component: ProfileLanguagesComponent
+  },
+  {
+    path: 'languages/new',
+    component: ProfileLanguagesEditComponent
+  },
+  {
+    path: 'languages/:uid/edit',
+    component: ProfileLanguagesEditComponent
   }
   ];
 
