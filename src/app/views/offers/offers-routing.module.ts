6cd24d5f38f902f8f58e2d4da3bd7194c066e755
Bruno Orcha García <borcha@uoc.edu>
2019-10-19 19:14:20 +0200

    Se agregan rutas para el módulo de ofertas

--- src/app/views/offers/offers-routing.module.ts
+++ src/app/views/offers/offers-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OffersComponent } from './offers.component';
+import { OffersDetailComponent } from './offers-detail/offers-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersComponent
+  },
+
+  {
+    path: ':id',
+    component: OffersDetailComponent
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class OffersRoutingModule { }
+
+
