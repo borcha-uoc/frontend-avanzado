ec45ee7e89791103501c8051a4589fbc0e1717e0
Bruno Orcha García <borcha@uoc.edu>
2019-10-19 16:10:53 +0200

    Se agrega formulario básico de edición de idiomas

--- src/app/views/profile/profile-languages-edit/profile-languages-edit.component.ts
+++ src/app/views/profile/profile-languages-edit/profile-languages-edit.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Language, LanguageLevel, LanguageName } from 'src/app/shared/models/language.model';
 
 @Component({
   selector: 'app-profile-languages-edit',
   templateUrl: './profile-languages-edit.component.html',
   styleUrls: ['./profile-languages-edit.component.scss']
 })
-export class ProfileLanguagesEditComponent implements OnInit {
+export class ProfileLanguagesEditComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  editing: boolean;
+  languagesForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder)
+  {
+    let language = new Language();
+    this.languagesForm = this.createFormGroup(language);
   }
 
+  createFormGroup(language) {
+    return this.formBuilder.group({
+      name: [language.name],
+      other: [null, Validators.required],
+      level: [language.level],
+      date: [language.date],
+    });
+  }
+
+  }
+
+  save() {
+    console.log(this.languagesForm.value);
+  }
 }
