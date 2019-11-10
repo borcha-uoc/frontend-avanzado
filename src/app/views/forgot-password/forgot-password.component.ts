528e314d9459c816a4b902c44de03f217ac84811
Bruno Orcha García <borcha@uoc.edu>
2019-11-10 23:01:58 +0100

    Se agrega módulo inicial de recuperar contraseña

--- src/app/views/forgot-password/forgot-password.component.ts
+++ src/app/views/forgot-password/forgot-password.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SigninService } from 'src/app/shared/services/signin.service'
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent {
 
-  constructor() { }
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
 
-  ngOnInit() {
+  forgotPasswordForm: FormGroup = this.builder.group({
+    email: this.email,
+  });
+
+  constructor(
+    private builder: FormBuilder,
+    private router: Router,
+    ) { }
+
+  recoverPassword() {
+    alert('solicitar recuperar contraseña');
   }
 
 }
--- src/app/views/forgot-password/forgot-password.module.ts
+++ src/app/views/forgot-password/forgot-password.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { ForgotPasswordComponent } from './forgot-password.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ForgotPasswordComponent],
-  imports: [
-    CommonModule,
-    ForgotPasswordRoutingModule
-  ]
+  imports: [SharedModule, ForgotPasswordRoutingModule ]
 })
 export class ForgotPasswordModule { }
