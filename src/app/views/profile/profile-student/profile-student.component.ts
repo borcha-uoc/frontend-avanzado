bc98f8331932e68cc0c80248d6f2c5352dce29ac
Bruno Orcha García <borcha@uoc.edu>
2019-11-12 01:18:51 +0100

    Se modifica el componente que muestra el perfil para solicitar la carga mediante un action, y obtene el usuario a partir de la suscripción.

--- src/app/views/profile/profile-student/profile-student.component.ts
+++ src/app/views/profile/profile-student/profile-student.component.ts
@@ -1,36 +1,52 @@
-import { Component } from '@angular/core';
-import { ProfileService } from '../../../shared/services/profile.service';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/shared/models/user.model';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IStore } from '@app/shared/state/store.interface';
+import { ProfileActions } from '@app/shared/state/user/actions';
+import { getUserProfile } from '@app/shared/state/user/selectors';
+
+
+
 
 @Component({
   selector: 'app-profile-student',
   templateUrl: './profile-student.component.html',
   styleUrls: ['./profile-student.component.scss']
 })
-export class ProfileStudentComponent {
-  user: User;
-  constructor(private profileService: ProfileService) {
-    this.user = this.profileService.user;
+export class ProfileStudentComponent implements OnInit {
+  user: Observable<User>;
+
+  constructor(private store: Store<IStore>) {
+    this.user = store.select(getUserProfile);
+  }
+
+  ngOnInit() {
+    this.store.dispatch(ProfileActions.loadUser());
   }
 
   deleteStudy(studyID: number) {
-    const studies = this.user.studies;
-    const index = studies.findIndex(study => study.uid === studyID);
-    if (index === -1) {
-      alert('Error: Study not found');
-      return;
-    }
-    studies.splice(index, 1);
-    this.profileService.updateProfile(this.user);
+    // this.store.dispatch(StudiesAction.delete({id:studyId}));
+
+    // const studies = this.user.studies;
+    // const index = studies.findIndex(study => study.uid === studyID);
+    // if (index === -1) {
+    //   alert('Error: Study not found');
+    //   return;
+    // }
+    // studies.splice(index, 1);
+    // this.profileService.updateProfile(this.user);
   }
   deleteLanguage(languageID: any) {
-    const languages = this.user.languages;
-    const index = languages.findIndex(language => language.uid === languageID);
-    if (index === -1) {
-      alert('Error: Language not found');
-      return;
-    }
-    languages.splice(index, 1);
-    this.profileService.updateProfile(this.user);
+    // this.store.dispatch(LanguagesAction.delete({id:languageID});
+
+    // const languages = this.user.languages;
+    // const index = languages.findIndex(language => language.uid === languageID);
+    // if (index === -1) {
+    //   alert('Error: Language not found');
+    //   return;
+    // }
+    // languages.splice(index, 1);
+    // this.profileService.updateProfile(this.user);
   }
 }
