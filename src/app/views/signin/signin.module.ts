cb27ff61195cd2b5352e0728086d013f857e37f4
Bruno Orcha García <borcha@uoc.edu>
2019-12-02 02:09:23 +0100

    Se rediseña la pantalla de login usando material

--- src/app/views/signin/signin.module.ts
+++ src/app/views/signin/signin.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { SigninRoutingModule } from './signin-routing.module';
 import { SigninComponent } from './signin.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatCardModule} from '@angular/material/card'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [SigninComponent],
-  imports: [SharedModule, SigninRoutingModule],
+  imports: [SharedModule, SigninRoutingModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatCardModule, FlexLayoutModule],
   providers: []
 })
 export class SigninModule {}
