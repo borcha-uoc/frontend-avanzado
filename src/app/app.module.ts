bca5cf38be473b09be3c087e35007e2fed479995
Bruno Orcha García <borcha@uoc.edu>
2020-01-12 14:01:06 +0100

    Usamos el FakeBackendService si no estamos en producción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { environment } from '../environments/environment';
     CoreModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(FakeBackendService, {
+    environment.production ? [] : HttpClientInMemoryWebApiModule.forRoot(FakeBackendService, {
       dataEncapsulation: false
     }),
     BrowserAnimationsModule,
